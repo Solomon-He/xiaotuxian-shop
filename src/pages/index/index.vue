fc9c9c893316dba11267026a415cf66aba2037ca
hzj411 <134387346+hzj411@users.noreply.github.com>
2025-02-23 19:45:22 +0800

    🐳 chore: 通用组件自动导入

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,10 @@ import CustomNavbar from './componets/CustomNavbar.vue'
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar></CustomNavbar>
+  <!-- 自定义轮播图 -->
+  <XtxSwiper></XtxSwiper>
   <view class="index">index</view>
 </template>
 
